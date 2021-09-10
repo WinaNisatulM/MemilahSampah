755d909b18b816ce516d748fea809e9aaaefbfa2
WinaNisatulM <wnm.tp20@mail.mmtc.ac.id>
2021-09-10 16:20:34 +0700

    PilahSampah
    Tugas6

---
+++ Memilah Sampah/Assets/Scripts/DeteksiSampah.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeteksiSampah : MonoBehaviour
+{
+    public string nameTag;
+    public AudioClip audioBenar;
+    public AudioClip audioSalah;
+        private AudioSource MediaPlayerBenar;
+        private AudioSource MediaPlayerSalah;
+        public Text textScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MediaPlayerBenar = gameObject.AddComponent<AudioSource>();
+        MediaPlayerBenar.clip = audioBenar;
+
+        MediaPlayerSalah = gameObject.AddComponent<AudioSource>();
+        MediaPlayerSalah.clip = audioSalah;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals(nameTag))
+        {
+            DataScore.score += 25;
+            textScore.text = DataScore.score.ToString();
+            Destroy(collision.gameObject);
+            MediaPlayerBenar.Play();
+        }
+        else
+        {
+            DataScore.score -= 5;
+            textScore.text = DataScore.score.ToString();
+            Destroy(collision.gameObject);
+            MediaPlayerSalah.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Memilah Sampah/Assets/Scripts/GameOver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+      
+            if(timer > 5)
+            {
+                DataScore.score = 0;
+                SceneManager.LoadScene("1");
+            }
+      
+        
+    }
+}
